31d136326ccfa4c0ca7dbe528a4c2eccf12a48af
Peter Širanec <petosiso@gmail.com>
2025-09-06 17:26:13 +0200

    photocarousel

--- src/components/ui/PhotoCarousel/imgUrls.ts
+++ src/components/ui/PhotoCarousel/imgUrls.ts
@@ -5,9 +5,4 @@ export const imgUrls = [
   "/photocarousel/3.webp",
   "/photocarousel/4.webp",
   "/photocarousel/5.webp",
-  "/photocarousel/6.webp",
-  "/photocarousel/7.webp",
-  "/photocarousel/8.webp",
-  "/photocarousel/9.webp",
-  "/photocarousel/10.webp",
  ];
